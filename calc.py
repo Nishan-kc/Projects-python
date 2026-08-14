1c29b4e4a828ea62e631d601b8866e6905718037
Nishan-kc <neesankcey@gmail.com>
2026-08-14 14:54:55 +0545

    else added

--- calc.py
+++ calc.py
@@ -6,11 +6,13 @@ num2 = float(input("Enter second number: "))
 
 if (operator == "+"):
     print(f"sum of {num1} and {num2} is : ", num1 + num2)
-if (operator == "-"):
+elif (operator == "-"):
     print(f" ofsubtraction of {num1} and {num2} is : ", num1 - num2)
-if (operator == "*"):
+elif (operator == "*"):
    print(f"multiplication of {num1} and {num2} is : ", num1 * num2)
-if (operator == "/"):
+elif (operator == "/"):
     print(f"divison of {num1} and {num2} is : ", num1 / num2)
+else:
+    print(f"{operator} is not a valid operator")
 
 
